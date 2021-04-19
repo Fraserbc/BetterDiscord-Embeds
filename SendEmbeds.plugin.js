c7b08eba003f32114cd8ad66f3a54bc4b5e016df
Fraserbc <fraserbcprice@gmail.com>
2021-04-19 14:21:11 +0100

    Detect Shift+Enter the correct way

--- SendEmbeds.plugin.js
+++ SendEmbeds.plugin.js
@@ -1,13 +1,49 @@
 /**
  * @name SendEmbeds
  * @author Fraserbc
- * @version 2.0.0
+ * @version 2.1.0
  * @description Allows you to send embeds
- * @source https://github.com/Fraserbc/BetterDiscord-Embeds
- * @updateUrl https://raw.githubusercontent.com/Fraserbc/BetterDiscord-Embeds/master/SendEmbeds.plugin.js
+ * @website https://github.com/Fraserbc
+ * @source https://github.com/Fraserbc/BetterDiscord-Embeds/blob/master/SendEmbeds.plugin.js
  */
+ 
+ const config = {
+    info: {
+        name: 'SendEmbeds',
+        authors: [
+            {
+                name: 'Fraser Price',
+                github_username: 'Fraserbc'
+            }
+        ],
+        version: '2.1.0',
+        description: 'Allows you to send embeds',
+        github: 'https://github.com/Fraserbc/BetterDiscord-Embeds',
+        github_raw: 'https://raw.githubusercontent.com/Fraserbc/BetterDiscord-Embeds/master/SendEmbeds.plugin.js'
+    }
+};
 
- module.exports = class {
+module.exports = class {
+    constructor() {
+        this._config = config;
+    }
+
+    getName() {
+        return config.info.name;
+    }
+
+    getAuthor() {
+        return config.info.authors.map(author => author.name).join(', ');
+    }
+
+    getDescription() {
+        return config.info.description;
+    }
+
+    getVersion() {
+        return config.info.version;
+    }
+
 	load() {}
 	unload() {}
 	start() { this.attachHandler(); }
@@ -17,10 +53,8 @@
 		if (el.length == 0) return;
 
 		// Remove handlers and injected script
-		el[0].removeEventListener("keydown", this.handler);
+		el[0].removeEventListener('keydown', this.handler);
 	}
-
-	lastKey = 0;
 
 	attachHandler() {
 		this.handler = this.handleKeypress.bind(this);
@@ -28,26 +62,26 @@
 		if (el.length == 0) return;
 	
 		// Bind the handler
-		el[0].addEventListener("keydown", this.handler, false);
+		el[0].addEventListener('keydown', this.handler, false);
 	}
 
 	// Function that sends the embed
 	sendEmbed(embed) {
-		// Get the ID of the channel we want ot send the embed to
-		let channelID = window.location.pathname.split('/').pop();
+		// Get the ID of the channel we want to send the embed
+		let channelID = BdApi.findModuleByProps('getChannelId').getChannelId();
 	
 		// Create the message
-		let MessageQueue = DiscordInternals.WebpackModules.findByUniqueProperties(['enqueue']);
-		let MessageParser = DiscordInternals.WebpackModules.findByUniqueProperties(['createBotMessage']);
+		let MessageQueue = BdApi.findModuleByProps('enqueue');
+		let MessageParser = BdApi.findModuleByProps('createBotMessage');
 	
-		let msg = MessageParser.createBotMessage(channelID, "");
+		let msg = MessageParser.createBotMessage(channelID, '');
 	
 		// Send the message
 		MessageQueue.enqueue({
 			type: 0,
 			message: {
 				channelId: channelID,
-				content: "",
+				content: '',
 				tts: false,
 				nonce: msg.id,
 				embed: embed
@@ -62,12 +96,10 @@
 		var code = e.keyCode || e.which;
 
 		if (code !== 13) {
-			this.lastKey = code;
 			return;
 		}
 
-		//Catch Shift + Enter and allow multiline
-		if (this.lastKey == 16) {
+		if (e.shiftKey) {
 			return;
 		}
 
@@ -90,7 +122,7 @@
 
 		// Parse the text
 		let elements = Array.from(document.querySelectorAll('div[class^="textArea-')[0].children[0].children);
-		let text = "";
+		let text = '';
 		elements.forEach(function(l0) {
 			Array.from(l0.children).forEach(function(l1) {
 				Array.from(l1.children).forEach(function(elem) {
@@ -102,10 +134,10 @@
 					}
 				});
 			});
-			text += "\n";
+			text += '\n';
 		});
 
-		if (!text.startsWith("/e") && !text.startsWith("/embed")) {
+		if (!text.startsWith('/e')) {
 			return;
 		};
 
@@ -114,23 +146,23 @@
 		e.stopPropagation();
 
 		// Strip and split the text
-		text = text.replace("/e ", "");
-		text = text.replace("\uFEFF", "");
-		text = text.replace(/\n\n/g, "\n");
-		text = text.split("\n");
+		text = text.replace('/e ', '');
+		text = text.replace('\uFEFF', '');
+		text = text.replace(/\n\n/g, '\n');
+		text = text.split('\n');
 
 		// Create the embed
-		let fields = ["title", "description", "url", "color", "timestamp", "footer_image", "footer", "thumbnail", "image", "author", "author_url", "author_icon"];
+		let fields = ['title', 'description', 'url', 'color', 'timestamp', 'footer_image', 'footer', 'thumbnail', 'image', 'author', 'author_url', 'author_icon'];
 		let embed = {};
-		let last_attrb = ""
+		let last_attrb = ''
 		for (var x = 0; x < text.length; x++) {
 			let line = text[x]
-			let split = splitSingle(line, ":");
+			let split = splitSingle(line, ':');
 
 			// Check if it is an attribute or continuation of previous
 			if(fields.includes(split[0])) {
-				// Check if there is a leading " "
-				if(split[1].startsWith(" ")) {
+				// Check if there is a leading ' '
+				if(split[1].startsWith(' ')) {
 					embed[split[0]] = split[1].slice(1);
 				} else {
 					embed[split[0]] = split[1];
@@ -139,7 +171,7 @@
 				// Store the last attribute to be set so we can have multi-line
 				last_attrb = split[0];
 			} else {
-				embed[last_attrb] += "\n" + line;
+				embed[last_attrb] += '\n' + line;
 			}
 		}
 
@@ -147,7 +179,7 @@
 		let unused = [];
 		let keys = Object.keys(embed);
 		for (var x = 0; x < keys.length; x++) {
-			if (embed[keys[x]] == "") {
+			if (embed[keys[x]] == '') {
 				unused.push(keys[x]);
 			}
 		}
@@ -158,44 +190,44 @@
 		}
 
 		// Proccess color
-		embed.color = embed.color ? parseInt(embed.color.replace("#", ""), 16) : 0;
+		embed.color = embed.color ? parseInt(embed.color.replace('#', ''), 16) : 0;
 
 		// Convert the embed to Discord's format
 		let discordEmbed = {
-			type: "rich",
-			footer: { text: "" },
-			author: { name: "" }
+			type: 'rich',
+			footer: { text: '' },
+			author: { name: '' }
 		}
 		keys = Object.keys(embed);
 		for (var x = 0; x < keys.length; x++) {
 			switch(keys[x]) {
-				case "timestamp":
-					if (embed.timestamp.toLowerCase() == "true") {
+				case 'timestamp':
+					if (embed.timestamp.toLowerCase() == 'true') {
 						let timestamp = (new Date).toISOString();
 						discordEmbed.timestamp = timestamp;
 					}
 					break;
-				case "footer_image":
+				case 'footer_image':
 					discordEmbed.footer.icon_url = embed.footer_image;
 					break;
-				case "footer":
+				case 'footer':
 					discordEmbed.footer.text = embed.footer;
 					break;
-				case "thumbnail":
+				case 'thumbnail':
 					discordEmbed.thumbnail = {};
 					discordEmbed.thumbnail.url = embed.thumbnail;
 					break;
-				case "image":
+				case 'image':
 					discordEmbed.image = {};
 					discordEmbed.image.url = embed.image;
 					break;
-				case "author":
+				case 'author':
 					discordEmbed.author.name = embed.author;
 					break;
-				case "author_url":
+				case 'author_url':
 					discordEmbed.author.url = embed.author_url;
 					break;
-				case "author_icon":
+				case 'author_icon':
 					discordEmbed.author.icon_url = embed.author_icon;
 					break;
 				default:
